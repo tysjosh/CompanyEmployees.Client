d3ee2b0dd67dab668d10147d0c42cad5292e13b8
Olatayo Joshua Olukotun <olukotunjosh@gmail.com>
2021-03-20 17:57:24 +0200

    adding xml media type

--- CompanyEmployees.Client/Services/HttpClientCrudService.cs
+++ CompanyEmployees.Client/Services/HttpClientCrudService.cs
@@ -1,9 +1,14 @@
 ﻿using Entities.DataTransferObjects;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace CompanyEmployees.Client.Services
 {
@@ -17,6 +22,11 @@ namespace CompanyEmployees.Client.Services
         {
 			_httpClient.BaseAddress = new Uri("https://localhost:5001/api/");
 			_httpClient.Timeout = new TimeSpan(0, 0, 30);
+			_httpClient.DefaultRequestHeaders.Clear();
+			_httpClient.DefaultRequestHeaders.Accept.Add(
+				new MediaTypeWithQualityHeaderValue("application/json"));
+			_httpClient.DefaultRequestHeaders.Accept.Add(
+				new MediaTypeWithQualityHeaderValue("text/xml"));
 			_options = new JsonSerializerOptions
 			{
 				PropertyNameCaseInsensitive = true
@@ -33,7 +43,26 @@ namespace CompanyEmployees.Client.Services
 			response.EnsureSuccessStatusCode();
 
 			var content = await response.Content.ReadAsStringAsync();
-			var companies = JsonSerializer.Deserialize<List<CompanyDto>>(content, _options);
+
+			var companies = new List<CompanyDto>();
+
+			if(response.Content.Headers.ContentType.MediaType == "application/json")
+            {
+				companies = JsonSerializer.Deserialize<List<CompanyDto>>(content, _options);
+			}
+			else if(response.Content.Headers.ContentType.MediaType == "text/xml")
+            {
+				var doc = XDocument.Parse(content);
+				foreach(var element in doc.Descendants())
+                {
+					element.Attributes().Where(a => a.IsNamespaceDeclaration).Remove();
+					element.Name = element.Name.LocalName;
+                }
+
+				var serializer = new XmlSerializer(typeof(List<CompanyDto>));
+				companies = (List<CompanyDto>)serializer.Deserialize(new StringReader(doc.ToString()));
+
+            }
 
         }
 	}
