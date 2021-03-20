16a583212c278478a847f8693d51afe2b7b5ecd4
Olatayo Joshua Olukotun <olukotunjosh@gmail.com>
2021-03-20 16:25:36 +0200

    implemment httpClient Service

--- CompanyEmployees.Client/Services/HttpClientCrudService.cs
+++ CompanyEmployees.Client/Services/HttpClientCrudService.cs
@@ -1,15 +1,40 @@
-﻿using System;
+﻿using Entities.DataTransferObjects;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace CompanyEmployees.Client.Services
 {
-	public class HttpClientCrudService : IHttpClientServiceImplementation
+    public class HttpClientCrudService : IHttpClientServiceImplementation
 	{
+        private static readonly HttpClient _httpClient = new HttpClient();
+		private readonly JsonSerializerOptions _options;
+
+
+		public HttpClientCrudService()
+        {
+			_httpClient.BaseAddress = new Uri("https://localhost:5001/api/");
+			_httpClient.Timeout = new TimeSpan(0, 0, 30);
+			_options = new JsonSerializerOptions
+			{
+				PropertyNameCaseInsensitive = true
+			};
+        }
 		public async Task Execute()
 		{
-			throw new NotImplementedException();
+			await GetCompanies();
 		}
+
+		public async Task GetCompanies()
+        {
+			var response = await _httpClient.GetAsync("companies");
+			response.EnsureSuccessStatusCode();
+
+			var content = await response.Content.ReadAsStringAsync();
+			var companies = JsonSerializer.Deserialize<List<CompanyDto>>(content, _options);
+
+        }
 	}
 }
